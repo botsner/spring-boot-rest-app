b5dbb626c0f98e4a4c661a0aafa7739b5f05aef5
Alexey Genze <botsner@yandex.ru>
2023-02-22 20:23:19 +0300

    Initial commit

---
+++ src/test/java/ru/botsner/springboot/rest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.botsner.springboot.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
